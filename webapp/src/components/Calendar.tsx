2169ff4c315c3d6b1d4b57d14f0a7d740aa6a547
Harold Alcala <harold.dev@gmail.com>
2022-11-01 08:06:27 +0800

    Calender

--- webapp/src/components/Calendar.tsx
+++ webapp/src/components/Calendar.tsx
@@ -2,44 +2,46 @@ import { Typography } from "@mui/material";
 import { DAYS_OF_WEEKS } from "../Constants";
 
 export default function Calendar({ title = "default title", days, startDayAtDayOfWeek }) {
-    const num_rows = Math.round(days.length / 7);
+  const num_rows = Math.ceil(days.length / 7);
 
-    console.log("num_rows:", num_rows, "startDayAtDayOfWeek:", startDayAtDayOfWeek, "days:", days);
+  console.log("num_rows:", num_rows, "startDayAtDayOfWeek:", startDayAtDayOfWeek, "days:", days);
 
-    function getDay(dayi, rowi) {
-      const canDisplay = day < days.length && ((rowi == 0 && dayi >= startDayAtDayOfWeek) || rowi > 0)
+  function getDay(dayi, rowi) {
+    const canDisplay = day < days.length && ((rowi == 0 && dayi >= startDayAtDayOfWeek) || rowi > 0);
 
-        return <div className={"calendar_day" + (dayi == 0 || dayi == 6 ? " callendar_weekend" : "")}>{day < days.length && canDisplay ?<div> {day++ +1}</div> : ""}</div>
-    }
+    return <div className={"calendar_day" + (dayi == 0 || dayi == 6 ? " calendar_weekend" : "")}>{day < days.length && canDisplay ? <div> {day++ + 1}</div> : ""}</div>;
+  }
 
-    let day = 0;
+  let day = 0;
 
-    return (
-        <div className="calendar_container">
-            <Typography>{title}</Typography>
+  return (
+    <div className="calendar_container">
+      <Typography>{title}</Typography>
 
-            <div className="calendar_row">
-                {DAYS_OF_WEEKS.map((_, dayi) => (
-                  <div key={dayi} className="calendar_day_head">{DAYS_OF_WEEKS[dayi]}</div>
-                ))}
-            </div>
+      <div className="calendar_row">
+        {DAYS_OF_WEEKS.map((_, dayi) => (
+          <div key={dayi} className="calendar_day_head">
+            {DAYS_OF_WEEKS[dayi]}
+          </div>
+        ))}
+      </div>
 
+      {"1"
+        .repeat(num_rows)
+        .split("1")
+        .map((row, rowi) => (
+          <div key={rowi} className="calendar_row">
             {"1"
-                .repeat(num_rows)
-                .split("1")
-                .map((row, rowi) => (
-                    <div key={rowi} className="calendar_row">
-                        {"1"
-                            .repeat(6)
-                            .split("1")
-                            .map((_, dayi) => (
-                                <div key={dayi} className="calendar_week">
-                                    {/* <div className={"calendar_day" + (dayi == 0 || dayi == 6 ? " callendar_weekend" : "")}>{day < days.length && ((rowi == 0 && dayi >= startDayAtDayOfWeek) || rowi > 0) ? day++ + 1 : ""}</div> */}
-                                    {getDay(dayi, rowi)}
-                                </div>
-                            ))}
-                    </div>
-                ))}
-        </div>
-    );
+              .repeat(6)
+              .split("1")
+              .map((_, dayi) => (
+                <div key={dayi} className="calendar_week">
+                  {/* <div className={"calendar_day" + (dayi == 0 || dayi == 6 ? " callendar_weekend" : "")}>{day < days.length && ((rowi == 0 && dayi >= startDayAtDayOfWeek) || rowi > 0) ? day++ + 1 : ""}</div> */}
+                  {getDay(dayi, rowi)}
+                </div>
+              ))}
+          </div>
+        ))}
+    </div>
+  );
 }
